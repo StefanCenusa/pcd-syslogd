db6dca72e13ec9b3912c9b45457490fc35b8582a
IOANA Stanescu <istanescu@bitdefender.com>
2018-05-23 17:17:24 +0300

    sql msg format

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const Syslogd = require('syslogd');
 
+let logs = [];
+function addToSql(obj){
+    let toAdd = {
+        delay: (obj.server04.time - obj.server01.time)*1000,
+        rtt12: (obj.server02.time - obj.server01.time)*1000,
+        rtt23: (obj.server03.time - obj.server02.time)*1000,
+        rtt34: (obj.server04.time - obj.server03.time)*1000,
+        source: (obj.server01.remote),
+        dest: (obj.server04.server)
+    };
+    console.log(JSON.stringify(toAdd,null,4));
+}
+
 Syslogd(info => {
     /*
     info = {
@@ -18,11 +31,44 @@ Syslogd(info => {
 
     const {tag, msg} = info;
 
-    if (tag.startsWith('server')) {
+    let servers = ['server01','server02','server03','server04'];
+    if (servers.indexOf(tag)!==-1) {
         const items = msg.split(' ');
         const [time, remote, server] = items;
+        let  i =0;
 
-    	console.log('sending to sql');
+        while(true){
+            if(i<logs.length){
+                if(logs[i].hasOwnProperty(tag)){
+                    i++;
+                    continue;
+                } else {
+                  logs[i][tag] = {
+                      time:time,
+                      remote:remote,
+                      server:server
+                  };
+                  break;
+                }
+
+            }    else {
+                let obj = {};
+                obj[tag] = {
+                    time:time,
+                    remote:remote,
+                    server:server
+                };
+                logs.push(obj);
+                break;
+            }
+        }
+
+        if(Object.keys(logs[0]).length === 4){
+            let to_add = logs.shift();
+            console.log('sending to sql');
+            addToSql(to_add);
+        }
+
     	console.log(tag, time, remote, server);
     }
 
